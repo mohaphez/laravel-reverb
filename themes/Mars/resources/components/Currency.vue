07bd0c1cb0d3a8c836ad695ce856da4b9f5b0977
Hessam Taghvaei <mohaphez@yahoo.com>
2024-03-30 19:53:31 +0330

    feat: Update Currency.vue component

--- themes/Mars/resources/components/Currency.vue
+++ themes/Mars/resources/components/Currency.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <div class="crypto-cards flex flex-wrap">
-      <div v-for="(currency, index) in cryptocurrencies" :key="index" class="crypto-card w-full md:w-1/3 lg:w-1/4 xl:w-1/5 mb-6 px-4">
-        <div class="bg-white ring ring-neutral-200 rounded-md p-4 transition-transform duration-300 transform hover:-translate-y-2">
-          <div class="flex justify-between items-center">
+  <div v-for="(exchange, index) in exchanges" :key="index">
+  <div class="mb-8">
+    <h1 class="text-3xl font-bold">{{ index }}</h1>
+    <hr class="my-2 border-neutral-300">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+      <template v-for="(currency, index) in exchange" :key="index">
+          <div class="bg-white rounded-md shadow-md p-5 transition-transform duration-300 transform hover:-translate-y-2">
+
             <h3 class="text-2xl font-semibold">{{ currency.currency }}</h3>
-            <p class="text-gray-600">{{ currency.amount }}</p>
+
+            <div class="flex justify-between items-center mt-2">
+              <p  class="text-xl font-semibold text-right">
+                {{ currency.amount }}
+              </p>
+              <p :class="{ 'text-green-500': currency.change > 0, 'text-red-500': currency.change < 0 }" class="text-sm text-gray-600">
+                <span v-if="currency.change > 0">&#9650;</span>
+                <span v-else-if="currency.change < 0">&#9660;</span>
+                {{ currency.change }}%
+              </p>
+            </div>
+
           </div>
-          <p :class="{ 'text-green-500': currency.change > 0, 'text-red-500': currency.change < 0 }" class="text-xl font-semibold text-right mt-auto">
-            <span v-if="currency.change > 0">&#9650;</span>
-            <span v-else-if="currency.change < 0">&#9660;</span>
-            {{ currency.change }}%
-          </p>
-        </div>
-      </div>
+      </template>
     </div>
+     </div>
   </div>
 </template>
 
@@ -23,38 +32,20 @@ export default {
   name: 'App',
   data() {
     return {
-      cryptocurrencies: []
+      exchanges: []
     };
   },
   mounted() {
-    // Fetch initial cryptocurrency data
-    this.fetchCryptocurrencyData();
-
-    // Listen for real-time updates using Laravel Echo
     window.Echo.channel('cryptocurrency-updates')
-      .listen('.Modules\\Currency\\Events\\Api\\V1\\CurrencyMarketUpdateEvent', (event) => {
-        this.cryptocurrencies = event.cryptocurrencies;
+      .listen('.Modules\\Currency\\Events\\V1\\CurrencyMarketUpdateEvent', (event) => {
+        this.exchanges = event.cryptocurrencies;
       });
-  },
-  methods: {
-    fetchCryptocurrencyData() {
-      // Make an HTTP GET request to your API route
-      fetch('/api/v1/webhook/handle')
-        .then(response => response.json())
-        .then(data => {
-          // Assign the retrieved data to the cryptocurrencies array
-          this.cryptocurrencies = data;
-        })
-        .catch(error => {
-          console.error('Error fetching cryptocurrency data:', error);
-        });
-    }
   }
 };
 </script>
 
 <style>
 body {
-  background-color: #f3f4f6; /* Equivalent to bg-neutral-100 in Tailwind */
+  background-color: #f3f4f6;
 }
 </style>
